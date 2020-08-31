8779fa95ac8d1994569b85c8657bc9290beb9d3b
EyupOzmen <ozmeneyup60@gmail.com>
2020-08-31 03:53:47 +0300

    "Exercise for React.Js"

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Footer = () => (
+
+    <div className="footer" >
+       <p>Coded for exercise by @EyupOzmen</p>
+       <p>Thanks <a href="https://www.youtube.com/watch?v=At_5xYramLk&t=812s">@Hamza Mirza</a> </p>
+    </div>
+)
+
+export default Footer
